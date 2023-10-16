a63f2dc0c5bb7e6763fd62412d2bd905f7a6105e
snitire <elvis.ritins@gmail.com>
2023-10-16 22:57:36 +0300

    initial speedtest object usage

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 import logging.config
 import yaml
 from configparser import ConfigParser
+from speedtest import Speedtest
 
 # When launched by run.py, the CWD does not change!!!
 CONFIG_PATH = "./config.ini"
@@ -14,6 +15,7 @@ logger = logging.getLogger(__name__)
 
 logger.info("Starting main app")
 logger.info("Reading values from config file")
+
 try:
     config = ConfigParser()
     config.read(CONFIG_PATH)
@@ -27,9 +29,20 @@ try:
     st_ipAddress = config.get("speedtest", "ipAddress")
     st_interface = config.get("speedtest", "interface")
 
+    speedtestExe = config.get("paths", "speedtestExe")
+
     logger.setLevel(logLevel)
 
     logger.debug(f"Read values interval={interval}, logLevel={logLevel}, logVerbosity={st_logVerbosity}, "
                  f"serverID={st_serverID}, hostname={st_hostname}, ipAddress={st_ipAddress}, interface={st_interface}")
 except:
     logger.critical(f"Unable to read values from config file at {CONFIG_PATH}: ", exc_info=1)
+    exit()
+else:
+    # Initialize object for interaction with the Speedtest exe
+    speedtest = Speedtest(speedtestExe, st_logVerbosity, st_serverID, st_hostname, st_ipAddress, st_interface)
+    logger.debug(f"Speedtest object created: {speedtest}")
+
+testResult = speedtest.runTest()
+
+logger.info(f"Received test results: {testResult}")
